24ee748c0c9db61e393ec70b10e8a6e2beea1768
Arjun Aditya <meow@nermalcat69.dev>
2025-09-11 13:50:49 +0530

    fix logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import Image from "next/image";
+
 import { TamboProvider } from "@tambo-ai/react";
 import { TamboMcpProvider } from "@tambo-ai/react/mcp";
 import { MessageThreadFull } from "@/components/tambo/message-thread-full";
 import { useMcpServers } from "@/components/tambo/mcp-config-modal";
 import { ChatInputProvider } from "@/contexts/chat-input-context";
 import { components, tools } from "@/lib/tambo";
+import { TamboLogo } from "@/components/tambo/logo";
 
 export default function Home() {
   const mcpServers = useMcpServers();
@@ -24,7 +25,9 @@ export default function Home() {
         <main className="bg-[#F3F8F5] h-screen flex flex-col">
           <div className="flex-1 flex flex-col pt-5 min-h-0">
             <div className="flex-1 flex flex-col w-4/5 mx-auto min-h-0">
-              <a href="https://tambo.co" target="_blank" className="mx-auto pb-10" ><Image src="./Tambo-Lockup.svg" width={200} height={200} alt="tambo logo" /></a>
+              <a href="https://tambo.co" target="_blank" className="mx-auto pb-10" >
+              <TamboLogo />
+              </a>
               <div className=" flex-shrink-0 p-4 bg-[#A1FCD1]/60 border rounded-t-xl border-b">
                 <h1 className="text-2xl font-semibold text-neutral-900 mb-2">
                   GitHub Explorer
